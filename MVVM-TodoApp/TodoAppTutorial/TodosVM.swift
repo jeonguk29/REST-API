a3038afb8cb10f2eaa24b3c7dcf8992acc8df641
jeonguk29 <jeonguk29@naver.com>
2024-08-01 16:02:25 +0900

    MVVM TodoApp with REST API : MVC to MVVM
    클로저를 활용하여 이벤트 처리 클로저를 뷰모델에 선언 해두고 todos의 값이 바뀌면 호출
    호출시 어떤 로직(값)을 실행할지는 뷰컨에서 정의
    
            // 사실상 뷰모델에 클로저의 타입을 정의하고 값을 여기서 정의하여 함수를 실행하는 것
            self.todosVM.notifyTodosChanged = { updatedTodos in
                self.todos = updatedTodos
                DispatchQueue.main.async {
                    self.myTableView.reloadData()
                }
            }

--- MVVM-TodoApp/TodoAppTutorial/TodosVM.swift
+++ MVVM-TodoApp/TodoAppTutorial/TodosVM.swift
@@ -9,13 +9,51 @@ import Foundation
 import Combine
 class TodosVM {
     
-
+    // MARK: - step2
+    // 가공된 최종 데이터
+    var todos : [Todo] = [] {
+        didSet {
+            print(#fileID, #function, #line, "- ")
+            self.notifyTodosChanged?(todos)
+        }
+    }
+    
+    // MARK: - step3
+    // 데이터 변경 이벤트 - 클로저로 이벤트를 전달해주는 것임 변경되었다고
+    var notifyTodosChanged : (([Todo]) -> Void)? = nil
     
     init(){
         print(#fileID, #function, #line, "- ")
-     
-
+        
+        fetchTodos()
     }
+    
+    
+    // MARK: - step1
+    /// 할일 가져오기
+    /// - Parameter page: 페이지
+    func fetchTodos(page: Int = 1){
+        print(#fileID, #function, #line, "- <#comment#>")
+        
+
+        // 서비스 로직
+        TodosAPI.fetchTodos(page: page, completion: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                
+                if let fetchedTodos : [Todo] = response.data{
+                    self.todos = fetchedTodos
+                }
+            case .failure(let failure):
+                print("failure: \(failure)")
+                
+            }
+            
+        })
+        
+    }
+    
     
     /// API 에러처리
     /// - Parameter err: API 에러
